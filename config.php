b7a20f3672da3678aba823ac1dc997644b4640ec
Dalton Edwards <daltontastic@gmail.com>
2016-03-22 23:04:20 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+# Server Info
+define("IP", "play.avengetech.net");
+define("PORT", "19132");
+
+# Twitter Tokens
+define("ACCESS_TOKEN", "Claim at apps.twitter.com");
+define("ACCESS_SECRET", "Claim at apps.twitter.com");
+define("CONSUMER_KEY", "Claim at apps.twitter.com");
+define("CONSUMER_SECRET", "Claim at apps.twitter.com");
+
+# Tweets
+define("TWEET_ONLINE", "Players:\n$players_online/$players_max\n\nVersion:\n$version");
+define("TWEET_OFFLINE", "Server is offline or unreachable.");
+?>
