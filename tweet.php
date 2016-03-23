7b71f5eaa32d83b4a8bf75ce682a4f00c9d8d28e
Dalton Edwards <daltontastic@gmail.com>
2016-03-22 23:04:45 -0400

    Update tweet.php

--- tweet.php
+++ tweet.php
@@ -1,39 +1,28 @@
 <?php
-# Input your server IP and port :)
-$server_IP   = "play.lbsg.net";
-$server_Port = "19132";
-
-# You probably shouldn't touch these variables. Move along!
-$query_JSON          = json_decode(file_get_contents("https://mcapi.ca/query/$server_IP:$server_Port/mcpe"), true);
-$query_OnlineStatus  = $query_JSON["status"];
-$query_ServerVersion = $query_JSON["version"];
-$query_OnlinePlayers = $query_JSON["players"]["online"];
-$query_MaxPlayers    = $query_JSON["players"]["max"];
-
-# Twitter API Tokens
-$your_ConsumerKey       = "blank";
-$your_ConsumerSecret    = "blank";
-$your_AccessToken       = "blank";
-$your_AccessTokenSecret = "blank";
-
-# Tweets
-if ($query_OnlineStatus == TRUE) {
-	# YAY! Your server is online! Tweet about it :)
-    $your_Tweet = "Connected Players\n$query_OnlinePlayers/$query_MaxPlayers\n\nVersion\n$query_ServerVersion";
-} else {
-	# The script is having trouble querying your server. Tweet about it :(
-    $your_Tweet = "Server is offline or unreachable.";
-}
-
-# UNLESS YOU KNOW WHAT YOU'RE DOING - DON'T EDIT ANYTHING BELOW THIS LINE! #
+require_once("config.php");
 require_once('codebird/codebird.php');
 
-\Codebird\Codebird::setConsumerKey("$your_ConsumerKey", "$your_ConsumerSecret");
+$ip   = IP;
+$port = PORT;
+
+$query          = json_decode(file_get_contents("https://mcapi.ca/query/$ip:$port/mcpe"), true);
+$status         = $query["status"];
+$version        = $query["version"];
+$players_online = number_format($query["players"]["online"]);
+$players_max    = number_format($query["players"]["max"]);
+
+if ($status == TRUE) {
+    $tweet = TWEET_ONLINE;
+} else {
+    $tweet = TWEET_OFFLINE;
+}
+
+\Codebird\Codebird::setConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
 $cb = \Codebird\Codebird::getInstance();
-$cb->setToken("$your_AccessToken", "$your_AccessTokenSecret");
+$cb->setToken(ACCESS_TOKEN, ACCESS_SECRET);
 
 $params = array(
-    'status' => "$your_Tweet"
+    'status' => "$tweet"
 );
 $reply  = $cb->statuses_update($params);
 ?>
