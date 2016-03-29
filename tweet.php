1e9e86cfe96dcfd9b37b3c38032b00e146c0f83e
Dalton Edwards <daltontastic@gmail.com>
2016-03-29 14:25:10 -0400

    fixed

--- tweet.php
+++ tweet.php
@@ -1,38 +1,33 @@
 <?php
-require_once('codebird/codebird.php');
-
-# Server Info
-define("IP", "play.avengetech.net");
-define("PORT", "19132");
-$ip   = IP;
-$port = PORT;
-
-$query          = json_decode(file_get_contents("https://mcapi.ca/query/$ip:$port/mcpe"), true);
-$status         = $query["status"];
-$version        = $query["version"];
-$players_online = number_format($query["players"]["online"]);
-$players_max    = number_format($query["players"]["max"]);
-
+# Input your server IP and port :)
+$server_IP              = "play.avengetech.net";
+$server_Port            = "19132";
+# You probably shouldn't touch these variables. Move along!
+$query_JSON             = json_decode(file_get_contents("https://mcapi.ca/query/$server_IP:$server_Port/mcpe"), true);
+$query_OnlineStatus     = $query_JSON["status"];
+$query_ServerVersion    = $query_JSON["version"];
+$query_OnlinePlayers    = $query_JSON["players"]["online"];
+$query_MaxPlayers       = $query_JSON["players"]["max"];
+# Twitter API Tokens
+$your_ConsumerKey       = "blank";
+$your_ConsumerSecret    = "blank";
+$your_AccessToken       = "blank";
+$your_AccessTokenSecret = "blank";
 # Tweets
-define("TWEET_ONLINE", "Players:\n$players_online/$players_max\n\nVersion:\n$version");
-define("TWEET_OFFLINE", "Server is offline or unreachable.");
-if ($status == TRUE) {
-    $tweet = TWEET_ONLINE;
+if ($query_OnlineStatus == TRUE) {
+    # YAY! Your server is online! Tweet about it :)
+    $your_Tweet = "Connected Players\n$query_OnlinePlayers/$query_MaxPlayers\n\nVersion\n$query_ServerVersion";
 } else {
-    $tweet = TWEET_OFFLINE;
+    # The script is having trouble querying your server. Tweet about it :(
+    $your_Tweet = "Server is offline or unreachable.";
 }
-
-# Twitter Tokens
-define("CONSUMER_KEY", "Claim at apps.twitter.com");
-define("CONSUMER_SECRET", "Claim at apps.twitter.com");
-define("ACCESS_TOKEN", "Claim at apps.twitter.com");
-define("ACCESS_SECRET", "Claim at apps.twitter.com");
-\Codebird\Codebird::setConsumerKey(CONSUMER_KEY, CONSUMER_SECRET);
+# UNLESS YOU KNOW WHAT YOU'RE DOING - DON'T EDIT ANYTHING BELOW THIS LINE! #
+require_once('codebird/codebird.php');
+\Codebird\Codebird::setConsumerKey("$your_ConsumerKey", "$your_ConsumerSecret");
 $cb = \Codebird\Codebird::getInstance();
-$cb->setToken(ACCESS_TOKEN, ACCESS_SECRET);
-
+$cb->setToken("$your_AccessToken", "$your_AccessTokenSecret");
 $params = array(
-    'status' => "$tweet"
+    'status' => "$your_Tweet"
 );
 $reply  = $cb->statuses_update($params);
 ?>
